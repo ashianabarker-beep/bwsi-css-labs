e5aa8b8099ce743224c2dacde9a3ad2c6e2036bb
ashianabarker-beep <ashianabarker@gmail.com>
2026-03-04 20:01:19 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Ashiana Barker" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+print ("Hi everyone, my name is Ashiana and I'm a freshman from Massachusetts! My favorite subjects right now are physics and AP environmental science. Outside of school, I love to bake and play piano. Right now, I'm practicing Gigue en Rondeau, and I highly recommend that you listen to it! I'm very excited to learn with all of you.")
 if __name__ == "__main__":
     main()
